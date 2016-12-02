d7179498888917e609aefc9ee1c77f67727c5760
James Calcaben <jcalcaben@magento.com>
2016-12-02 10:14:56 -0600

    Create new conversion script using new classes

---
+++ scripts/convert-excel-to-xml.py
@@ -0,0 +1,36 @@
+#!/user/bin/python
+
+import sys,os,getopt
+
+from classes.XmlConverter import XmlConverter
+from classes.ExcelParser import ExcelParser
+
+if __name__ == "__main__":
+    inputfile = None
+    outputfile = None
+    try:
+        opts,args = getopt.getopt(sys.argv[1:],"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        print 'xml-export.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofile"):
+            outputfile = arg
+
+    if(inputfile is not None):
+        parser = ExcelParser(os.getcwd(),inputfile=inputfile)
+
+        terms = parser.getTerms()
+
+        converter = XmlConverter("terms")
+
+        for term in terms:
+            converter.addTerm(term)
+
+        print converter.toString()
+
