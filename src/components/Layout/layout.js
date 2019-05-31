218cb7a51b15a5b1c1a2c69de6babfe346d63526
Eugene Bannykh <ybannykh@magento.com>
2019-05-31 17:04:54 -0500

    Add favicon to Layout

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -8,6 +8,23 @@ import '../../templates/globals.css'
 
 import styles from './layout.module.css'
 
+const Favicon = () => {
+  return(
+    <Helmet>
+      <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="https://magento.com/favicon.ico" />
+      <link rel="apple-touch-icon" type="image/png" href="https://magento.com/apple-touch-icon.png" />
+      <link rel="apple-touch-icon" type="image/png" sizes="57x57" href="https://magento.com/apple-touch-icon-57x57.png" />
+      <link rel="apple-touch-icon" type="image/png" sizes="72x72" href="https://magento.com/apple-touch-icon-72x72.png" />
+      <link rel="apple-touch-icon" type="image/png" sizes="76x76" href="https://magento.com/apple-touch-icon-76x76.png" />
+      <link rel="apple-touch-icon" type="image/png" sizes="114x114" href="https://magento.com/apple-touch-icon-114x114.png" />
+      <link rel="apple-touch-icon" type="image/png" sizes="120x120" href="https://magento.com/apple-touch-icon-120x120.png" />
+      <link rel="apple-touch-icon" type="image/png" sizes="144x144" href="https://magento.com/apple-touch-icon-144x144.png" />
+      <link rel="apple-touch-icon" type="image/png" sizes="152x152" href="https://magento.com/apple-touch-icon-152x152.png" />
+      <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="https://magento.com/apple-touch-icon-180x180.png" />
+    </Helmet>
+  );
+}
+
 const Layout = props => {
   const { children, currentPageUrl } = props
   const [showPanel, setPanel] = useState(false)
@@ -44,6 +61,7 @@ const Layout = props => {
         title={site.siteMetadata.title}
         meta={[{ name: 'description', content: site.siteMetadata.description }]}
       />
+      <Favicon />
 
       <div className={styles.container}>
         <header>
